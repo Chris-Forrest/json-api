50557f85b5a57fa172700bce2e0dca8203b35a36
Chris Forrest <redrider_cr@yahoo.com>
2021-11-12 15:36:28 -0500

    made a dotenv file small changes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const server = express();
+const port = process.env.PORT || 3000;
+
 
 server.use(bodyparser.json());
 
-server.listen(3000, () => {
-    console.log("Server started on port 3000")
+server.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`)
 });
