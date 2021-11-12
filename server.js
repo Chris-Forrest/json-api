10fc9a92dfa2d3d5f8ac6909b628d17b143ca56d
Chris Forrest <redrider_cr@yahoo.com>
2021-11-12 14:48:56 -0500

    adding dependencies setting up nodemon

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 const server = express();
 
-server.use(express.json());
+server.use(bodyparser.json());
 
 server.listen(3000, () => {
     console.log("Server started on port 3000")
